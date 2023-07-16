096dae74d68beb6abca57ba30da0a56823ad739c
NAT2K15 <72643615+NAT2K15@users.noreply.github.com>
2023-07-15 23:59:44 -0400

    Reworked the functions
    - Reworked the functions
    - Auto creates cars folder
    - Updated the exe

--- README.md
+++
@@ -1,5 +0,0 @@
-# vehicle-merger
-A simple script that will merger all your vehicles into one. There are multipe reasons to do this. This increases performances and also makes you have less resources.
-
-### Video showcase
-placeholder
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
-const chalk = require('chalk')
-let vehicleList = []
+const chalk = require('chalk');
+
+let vehicleList = [];
+let logStream;
 
 console.log(chalk.red.bold`
 ███████╗██╗██╗   ██╗███████╗███╗   ███╗    ██╗   ██╗███████╗██╗  ██╗██╗ ██████╗██╗     ███████╗    ███╗   ███╗███████╗██████╗  ██████╗ ███████╗██████╗ 
@@ -14,136 +16,197 @@ Thank you for using this script!
 - Discord: nat2k15
 - Github: https://github.com/nat2k15
 - Discord Server: https://discord.gg/RquDVTfDwu
-- Website: https://store.nat2k15.xyz\n\n` + chalk.yellow.bold `Starting the script...`)
-main()
-async function main() {
-  console.log(chalk.yellow.bold `Checking for exports folder...`)
-  createExport()
-  console.log(chalk.green.bold `Finished creating the exports folder...` + chalk.white `Done!`)
-  await delay(2000)
-  console.log(chalk.yellow.bold `Checking for stream/data folders...`)
-  exportStream()
-  console.log(chalk.green.bold `Finished exporting all the vehicles into ${__dirname}/exports/` + chalk.white `Done!`)
-  await delay(2000)
+- Website: https://store.nat2k15.xyz\n\n` + chalk.yellow.bold `Starting the script...`);
 
-  console.log(chalk.yellow.bold `Converting vehicleList into a json file...`)
-  fs.writeFileSync('exports/spawncodes.json', JSON.stringify(vehicleList))
-  console.log(chalk.green.bold `Finished converting vehicleList into a json file...` + chalk.white `Done!`)
-  console.log(chalk.red`
+main();
+
+async function main() {
+  console.log(chalk.yellow.bold `Checking for cars folder...`);
+  await delay(1000);
+  if(await checkCars()) {
+    console.log(chalk.green.bold `Found cars folder!` + chalk.white ` Done!`);
+  } else {
+    console.log(chalk.red.bold `Cars folder was not found but has been created please put vehicles in the cars folder and re run the script.`);
+    await delay(3000)
+    process.exit(10)
+  }
+  await delay(2000);
+  console.log(chalk.green.bold `Finished checking for cars folder...` + chalk.white `Done!`);
+
+  console.log(chalk.yellow.bold `Checking for exports folder...`);
+  await delay(1000);
+  createExport();
+  await delay(2000);
+  log(chalk.green.bold `Finished creating the exports folder...` + chalk.white `Done!`);
+  await delay(1000);
+
+  log(chalk.yellow.bold `Checking for stream/data folders...`);
+  await delay(1000);
+  exportStream();
+  log(chalk.green.bold `Finished exporting all the vehicles into ${__dirname}/exports/` + chalk.white `Done!`);
+  await delay(2000);
+
+  log(chalk.yellow.bold `Converting vehicleList into a JSON file...`);
+  fs.writeFileSync('exports/spawncodes.json', JSON.stringify({ vehicles: vehicleList }, null, 2));
+  log(chalk.green.bold `Finished converting vehicleList into a JSON file...` + chalk.white `Done!`);
+  
+  log(chalk.red(`
   ████████╗██╗  ██╗ █████╗ ███╗   ██╗██╗  ██╗    ██╗   ██╗ ██████╗ ██╗   ██╗
   ╚══██╔══╝██║  ██║██╔══██╗████╗  ██║██║ ██╔╝    ╚██╗ ██╔╝██╔═══██╗██║   ██║
      ██║   ███████║███████║██╔██╗ ██║█████╔╝      ╚████╔╝ ██║   ██║██║   ██║
      ██║   ██╔══██║██╔══██║██║╚██╗██║██╔═██╗       ╚██╔╝  ██║   ██║██║   ██║
      ██║   ██║  ██║██║  ██║██║ ╚████║██║  ██╗       ██║   ╚██████╔╝╚██████╔╝
      ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝       ╚═╝    ╚═════╝  ╚═════╝ 
-    For using out vehicle merger. If you would like to support join our discord.                                                                       
+    For using our vehicle merger. If you would like to support, join our discord.                                                                       
     https://discord.gg/RquDVTfDwu
-  `)
-  await delay(1000)
-  console.log(chalk.red `Exiting...`)
-  await delay(3000)
-  
+  `));
+  await delay(1000);
+
+  log(chalk.red(`Exiting...`));
+  await delay(3000);
+  logStream.end();
 }
 
-async function createDir(name) {
-  if(!fs.existsSync(`exports/data/${name}`)) {
-      console.log(chalk.yellow.bold `Creating exports/data/${name} folder...`)
-      fs.mkdirSync(`exports/data/${name}`)
-      console.log(chalk.green `Finished exports/data/${name} folder...` + chalk.white `Done!`)
-  }
-  if(!fs.existsSync(`exports/stream/${name}`)) {
-      console.log(chalk.yellow.bold `Creating exports/stream/${name} folder...`)
-      fs.mkdirSync(`exports/stream/${name}`)
-      console.log(chalk.green `Finished exports/stream/${name} folder...` + chalk.white `Done!`)
-  }
+async function checkCars() {
+  if (!fs.existsSync('cars')) {
+    console.log(chalk.red.bold `Couldn't find ${'cars'} folder!`);
+    console.log(chalk.yellow.bold `Creating ${'cars'} folder...`);
+    fs.mkdirSync('cars');
+    console.log(chalk.green.bold `Finished creating ${'cars'} folder...` + chalk.white `Done!`);
+    return false;
+  } 
+  return true;
 }
 
 
 async function createExport() {
-  if(!fs.existsSync('exports')) {
-    console.log(chalk.yellow `Creating exports folder...`)
-    fs.mkdirSync('exports')
-    console.log(chalk.green `Finished exports folder...` + chalk.white `Done!`)
-    console.log(chalk.yellow.bold `Creating exports/stream folder...`)
-    fs.mkdirSync('exports/stream')
-    console.log(chalk.green `Finished exports/stream folder...` + chalk.white `Done!`)
-    console.log(chalk.yellow.bold `Creating exports/data folder...`)
-    fs.mkdirSync('exports/data')
-    console.log(chalk.green `Finished exports/data folder...` + chalk.white `Done!`)
-    console.log(chalk.green `Finished exports/fxmanifest.lua file...` + chalk.white `Done!`)
-    fs.writeFileSync('exports/fxmanifest.lua', `fx_version 'cerulean'\ngames { 'gta5' }\nfiles {\n'data/**/carcols.meta',\n'data/**/carvariations.meta',\n'data/**/handling.meta',\n'data/**/vehiclelayouts.meta',\n'data/**/vehicles.meta',\n}\ndata_file 'VEHICLE_LAYOUTS_FILE'    'data/**/vehiclelayouts.meta'\ndata_file 'HANDLING_FILE'            'data/**/handling.meta'\ndata_file 'VEHICLE_METADATA_FILE'    'data/**/vehicles.meta'\ndata_file 'CARCOLS_FILE'            'data/**/carcols.meta'\ndata_file 'VEHICLE_VARIATION_FILE'    'data/**/carvariations.meta'`)
+  if (!fs.existsSync('exports')) {
+    logStream = fs.createWriteStream('exports/log.txt');
+    log(chalk.yellow `Creating exports folder...`);
+    fs.mkdirSync('exports');
+    log(chalk.green `Finished exports folder...` + chalk.white `Done!`);
+
+    const exportsStreamFolderPath = 'exports/stream';
+    log(chalk.yellow.bold `Creating ${exportsStreamFolderPath} folder...`);
+    fs.mkdirSync(exportsStreamFolderPath);
+    log(chalk.green `Finished ${exportsStreamFolderPath} folder...` + chalk.white `Done!`);
+
+    const exportsDataFolderPath = 'exports/data';
+    log(chalk.yellow.bold `Creating ${exportsDataFolderPath} folder...`);
+    fs.mkdirSync(exportsDataFolderPath);
+    log(chalk.green `Finished ${exportsDataFolderPath} folder...` + chalk.white `Done!`);
+
+    const fxManifestPath = 'exports/fxmanifest.lua';
+    log(chalk.green `Finished ${fxManifestPath} file...` + chalk.white `Done!`);
+    fs.writeFileSync(
+      fxManifestPath,
+      `fx_version 'cerulean'\ngames { 'gta5' }\nfiles {\n\t'data/**/carcols.meta',\n\t'data/**/carvariations.meta',\n\t'data/**/handling.meta',\n\t'data/**/vehiclelayouts.meta',\n\t'data/**/vehicles.meta',\n}\ndata_file 'VEHICLE_LAYOUTS_FILE'    'data/**/vehiclelayouts.meta'\ndata_file 'HANDLING_FILE'            'data/**/handling.meta'\ndata_file 'VEHICLE_METADATA_FILE'    'data/**/vehicles.meta'\ndata_file 'CARCOLS_FILE'            'data/**/carcols.meta'\ndata_file 'VEHICLE_VARIATION_FILE'    'data/**/carvariations.meta'`
+    );
   } else {
     fs.rm("exports", { recursive: true, force: true }, (err) => {
-      if(err) {
+      if (err) {
         console.log(chalk.red `Error removing the exports folder. ` + chalk.white `Please remove it manually and try again.\n${err.stack}`)
-      }    
-    })
-    await delay(1000)
-    createExport()
+      }
+    });
+    await delay(1000);
+    createExport();
   }
 }
+
+async function createDir(name) {
+  const dataFolderPath = `exports/data/${name}`;
+  const streamFolderPath = `exports/stream/${name}`;
+
+  if (!fs.existsSync(dataFolderPath)) {
+    log(chalk.yellow.bold `Creating ${dataFolderPath} folder...`);
+    fs.mkdirSync(dataFolderPath);
+    log(chalk.green `Finished ${dataFolderPath} folder...` + chalk.white `Done!`);
+  }
+  if (!fs.existsSync(streamFolderPath)) {
+    log(chalk.yellow.bold `Creating ${streamFolderPath} folder...`);
+    fs.mkdirSync(streamFolderPath);
+    log(chalk.green `Finished ${streamFolderPath} folder...` + chalk.white `Done!`);
+  }
+}
+
 async function exportStream() {
-    console.log(chalk.yellow.bold `Exporting stream and data folder...`);
-    try {
-      fs.readdirSync('cars').forEach(dir => {
-        if (!fs.lstatSync(`cars/${dir}`).isDirectory()) {
-          console.log(chalk.red `Error: ${dir} is not a directory!`)
-        } else {
-          fs.readdirSync(`cars/${dir}`).forEach(async mainroot => {
+  log(chalk.yellow.bold `Exporting stream and data folders...`);
+
+  try {
+    fs.readdirSync('cars').forEach(dir => {
+      if (!fs.lstatSync(`cars/${dir}`).isDirectory()) {
+        log(chalk.red `Error: ${dir} is not a directory!`);
+      } else {
+        fs.readdirSync(`cars/${dir}`).forEach(async mainroot => {
           await createDir(dir);
+
           if (!fs.lstatSync(`cars/${dir}/${mainroot}`).isDirectory()) {
             if (mainroot.endsWith('.meta')) {
               try {
                 fs.copyFileSync(`cars/${dir}/${mainroot}`, `exports/data/${dir}/${mainroot}`);
-                console.log(chalk.green `Exported ${mainroot} to exports/data/${dir}/`);
+                log(chalk.green `Exported ${mainroot} to exports/data/${dir}/`);
 
-                if(mainroot === 'vehicles.meta') {
-                  console.log(chalk.red.bold `Looking for the vehicle name in cars/${dir}/${mainroot}/vehicles.meta`)
+                if (mainroot === 'vehicles.meta') {
+                  log(chalk.red.bold `Looking for the vehicle name in cars/${dir}/${mainroot}/vehicles.meta`);
+
                   let data = fs.readFileSync(`cars/${dir}/${mainroot}`, 'utf8');
                   let modelName = data.match(/<modelName>(.*)<\/modelName>/)[1];
-                  console.log(chalk.green.bold `Found the vehicle name: ${modelName}`)
-                  vehicleList.push(modelName)
-                  console.log(chalk.yellow.bold `Adding the vehicle name to the list...`)
-                  console.log(chalk.green.bold `Added the vehicle name to the list!`)
+                  log(chalk.green.bold `Found the vehicle name: ${modelName}`);
+
+                  vehicleList.push(modelName);
+                  log(chalk.yellow.bold `Adding the vehicle name to the list...`);
+
+                  log(chalk.green.bold `Added the vehicle name to the list!`);
+
                 }
               } catch (error) {
-                console.error(chalk.red `Error copying file: ${error.message}`);
+                log(chalk.red `Error copying file: ${error.message}`);
+
               }
             }
           } else {
             if (mainroot === 'stream' || mainroot === 'data') {
               const folderType = mainroot === 'stream' ? 'stream' : 'data';
-              console.log(chalk.yellow `Exporting ${folderType} folder ${mainroot}...`);
+              log(chalk.yellow `Exporting ${folderType} folder ${mainroot}...`);
+
               try {
                 fs.readdirSync(`cars/${dir}/${mainroot}`).forEach(file => {
-                  if(file.endsWith('vehicles.meta')) {
-                    console.log(chalk.red.bold `Looking for the vehicle name...`)
+                  if (file.endsWith('vehicles.meta')) {
+                    log(chalk.red.bold `Looking for the vehicle name...`);
+
                     let data = fs.readFileSync(`cars/${dir}/${mainroot}`, 'utf8');
                     let modelName = data.match(/<modelName>(.*)<\/modelName>/)[1];
-                    console.log(chalk.green.bold `Found the vehicle name: ${modelName}`)
-                    vehicleList.push(modelName)
-                    console.log(chalk.yellow.bold `Adding the vehicle name to the list...`)
-                    console.log(chalk.green.bold `Added the vehicle name to the list!`)
+                    log(chalk.green.bold `Found the vehicle name: ${modelName}`);
+
+                    vehicleList.push(modelName);
+                    log(chalk.yellow.bold `Adding the vehicle name to the list...`);
+
+                    log(chalk.green.bold `Added the vehicle name to the list!`);
+
                   }
-                  console.log(chalk.yellow `Copying ${file} to exports/${folderType}/${dir}/`);
+                  log(chalk.yellow `Copying ${file} to exports/${folderType}/${dir}/`);
+
                   fs.copyFileSync(`cars/${dir}/${mainroot}/${file}`, `exports/${folderType}/${dir}/${file}`);
-                  console.log(chalk.green `Exported ${file} to exports/${folderType}/${dir}/` + chalk.white `Done!`);
+                  log(chalk.green `Exported ${file} to exports/${folderType}/${dir}/` + chalk.white `Done!`);
                 });
               } catch (error) {
-                console.error(`Error copying files in ${folderType} folder: ${error.message}`);
+                log(`Error copying files in ${folderType} folder: ${error.message}`);
               }
             }
           }
         });
-        }
-      });
-    } catch (error) {
-      console.error(`Error exporting stream folder: ${error.message}`);
-    }
+      }
+    });
+  } catch (error) {
+    log(`Error exporting stream folder: ${error.message}`);
   }
-  
-
+}
 
 function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function log(content) {
+  console.log(content)
+  logStream.write(content + '\n');
 }
